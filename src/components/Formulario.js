9a7a84964acea47c619ed17490ef536078d655ad
cr-urra <cr.urra6@gmail.com>
2021-02-11 17:21:51 -0300

    Se sube avances en componentes de credenciales a Zendesk

---
+++ src/components/Formulario.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export default class formulario extends Component {
+
+  state = {
+    url: "https://priority.zendesk.com/api/v2/groups.json",
+    user: "victor.olguin@priority.cl",
+    pwd: "Pr10r1ty2019"
+  }
+
+  componentDidMount = () => {
+    const auth = {
+      user: this.state.user, 
+      pwd: this.state.pwd
+    }
+    const res = axios.get(this.state.url, auth)
+    console.log(res);
+  }
+
+  render() {
+
+      return (
+          <div>
+              
+            </div>
+       )
+    }
+}
