d0608d271c88a872f8101e36ad10fb2f2d007c89
cr-urra <cr.urra6@gmail.com>
2021-02-15 15:09:26 -0300

    Se suben pruebas 2

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -8,15 +8,11 @@ export default class formulario extends Component {
     //console.log(res);
   }
   render() {
-
-      return (
-          <div>
-              <a href="https://priority.zendesk.com/oauth/authorizations/new?response_type=code&redirect_uri={https://cr-urra.github.io/priority-tickets-exports}&client_id={cd2eab47f51f1cbbb873ef116a239bb9c56dc08d4a714f09e27d17da2f5914fd}&scope=tickets:read">
-                <button type="button" className="btn btn-primary">Primary</button>
-              </a>
-              
-
-            </div>
-       )
+      return  <div>
+                <a href="https://priority.zendesk.com/oauth/authorizations/new">
+                  <button type="button" className="btn btn-primary">Primary</button>
+                </a>
+              </div>
+       
     }
 }
