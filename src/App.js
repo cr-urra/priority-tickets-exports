fd55a029a36b195c53c1104be26c33fe3934aa0b
cr-urra <cr.urra6@gmail.com>
2021-02-17 14:47:13 -0300

    Se sube front desarrollado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.jpg';
 
 import Formulario from './components/Formulario'
 
@@ -8,19 +8,14 @@ function App() {
 
   return <div className="App">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
+              <div className="row mb-4">
+                <div className="col-xs-4 col-md-4 col-lg-3">
+                  <img src={logo} Alt="Logo"></img>
+                </div>
+                <h1 className="col-xs-2 col-md-8 col-lg-9 mt8 pl-5">Exportador de Tickets</h1>
+              </div>
               <Formulario/>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
+              <h5 className="mt-3">© Copyright 2021 | Todos los derechos reservados a Priority Ltda.</h5>
             </header>
           </div>
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -4,14 +4,47 @@ import axios from 'axios'
 export default class formulario extends Component {
 
   componentDidMount = async () => {
-    //const res = await axios.get("https://priority.zendesk.com/oauth/authorizations/new?response_type=code&redirect_uri={https://cr-urra.github.io/priority-tickets-exports}&client_id={cd2eab47f51f1cbbb873ef116a239bb9c56dc08d4a714f09e27d17da2f5914fd}&scope=tickets:read")
-    //console.log(res);
+    let direction = 'http://localhost:3000/priority-tickets-exports'
+    let url = window.location.href;
+    if(url.indexOf(direction) !== -1){
+      if (url.indexOf('access_token=') !== -1){
+        let token = url.replace(direction+"#access_token=", "")
+        token = token.substr(0,token.indexOf("&"))
+        const res = await axios.get("https://priority.zendesk.com/api/v2/tickets.json?page=5",{
+          headers: {
+            Authorization: 'Bearer '+token
+          }
+        })
+        console.log(res);
+      }else if(url.indexOf('error=') !== -1){
+
+      }
+    }
   }
+  
   render() {
-      return  <div>
-                <a href="https://priority.zendesk.com/oauth/authorizations/new">
-                  <button type="button" className="btn btn-primary">Primary</button>
-                </a>
+      return  <div className="col-xs-8 col-md-8 col-lg-5">
+                <div className="card card-body">
+                  <h5 className="mb-5 text-left">Bienvenido a la herramienta de exportación de tickets. Para procesar una solicitud de forma correcta, por favor seguir los pasos descritos a continuación:</h5>
+                  <h6 className="mb-4 text-left">1) Haga clic en el siguiente botón de autenticar, lo redirigirá al formulario de autenticación de Zendesk para solicitar su Token de acceso (Su cuenta debe tener rol de agente o administrador):</h6>
+                  <a href="https://priority.zendesk.com/oauth/authorizations/new?response_type=token&client_id=priority_tickets_export&scope=read">
+                    <button type="button" className="btn btn-danger mb-4">Autenticar</button>
+                  </a>
+                  <h6 className="mb-4 text-left">2) Ingrese en el siguiente campo el nombre del cliente del que desea exportar sus tickets:</h6>
+                  <div className="form-group">
+                        <input type="Tel" className="form-control" placeholder="Ej: 123456789" name="rut" onChange={this.onInputChange} required/>
+                  </div>
+                  <h6 className="mb-4 text-left">3) Seleccione el formato del documento a exportar:</h6>
+                  <select className="mb-4 form-control">
+                    <option selected>Tipo de formato</option>
+                    <option value={1}>Formato PDF</option>
+                    <option value={2}>Formato XLSX</option>
+                  </select>
+                  <h6 className="mb-4 text-left">4) Haga clic en el siguiente botón de procesar solicitud, para realizar la exportación solicitada:</h6>
+                  <a href="https://priority.zendesk.com/oauth/authorizations/new?response_type=token&client_id=priority_tickets_export&scope=read">
+                    <button type="button" className="btn btn-danger mb-4">Procesar solicitud</button>
+                  </a>
+                </div>
               </div>
        
     }
